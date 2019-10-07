d7b4a1aa2d698b2cfc274e04999739d352b70652
Alan Ou <ipapple852@outlook.com>
2019-10-07 16:45:52 +0800

    Embed webview initial url in player.

--- lib/src/player.dart
+++ lib/src/player.dart
@@ -47,7 +47,7 @@ class __PlayerState extends State<_Player> with WidgetsBindingObserver {
     return IgnorePointer(
       ignoring: true,
       child: WebView(
-        initialUrl: player,
+        initialUrl: Uri.dataFromString(player, mimeType: 'text/html', encoding: Encoding.getByName('utf-8')).toString(),
         javascriptMode: JavascriptMode.unrestricted,
         initialMediaPlaybackPolicy: AutoMediaPlaybackPolicy.always_allow,
         javascriptChannels: {
@@ -186,7 +186,7 @@ class __PlayerState extends State<_Player> with WidgetsBindingObserver {
           widget.controller.value = widget.controller.value.copyWith(
             isEvaluationReady: true,
           );
-          if (widget.flags.forceHideAnnotation) {
+          if (Platform.isAndroid && widget.flags.forceHideAnnotation) {
             widget.controller.forceHideAnnotation();
           }
         },
@@ -195,13 +195,311 @@ class __PlayerState extends State<_Player> with WidgetsBindingObserver {
   }
 
   String get player {
-    String baseUrl = 'https://sarbagyadhaubanjar.github.io/youtube_player';
     if (Platform.isAndroid) {
-      return '$baseUrl/android';
+      return androidYoutubeIframePlayer;
     } else if (Platform.isIOS) {
-      return '$baseUrl/ios';
+      return iosYoutubeIframePlayer;
     } else {
       return 'https://flutter.io';
     }
+  }
+
+  String get androidYoutubeIframePlayer {
+    return 
+    '''
+<!DOCTYPE html>
+<html>
+  <head>
+    <style>
+      html, body {
+        height: 100%;
+        width: 100%;
+        margin: 0;
+        padding: 0;
+        background-color: #000000;
+        overflow: hidden;
+        position: fixed;
+      }
+    </style>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'>
+  </head>
+
+  <body>
+    <!-- 1. The <iframe> (and video player) will replace this <div> tag. -->
+    <div id="player"></div>
+
+    <script>
+      // 2. This code loads the IFrame Player API code asynchronously.
+      var tag = document.createElement('script');
+
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    </script>
+
+    <script type="text/javascript">
+      var player;
+      var timerId;
+      function onYouTubeIframeAPIReady() {
+          player = new YT.Player('player', {
+              height: '100%',
+              width: '100%',
+              host: 'https://www.youtube.com',
+              playerVars: {
+                  'controls': 0,
+                  'autoplay': 0,
+                  'playsinline': 1,
+                  'enablejsapi': 1,
+                  'fs': 0,
+                  'rel': 0,
+                  'showinfo': 0,
+                  'iv_load_policy': 3,
+                  'modestbranding': 1,
+                  'cc_load_policy': 0,
+              },
+              events: {
+                  onReady: function (event) { Ready.postMessage("Ready") },
+                  onStateChange: function (event) { sendPlayerStateChange(event.data) },
+                  onPlaybackQualityChange: function (event) { PlaybackQualityChange.postMessage(event.data) },
+                  onPlaybackRateChange: function (event) { PlaybackRateChange.postMessage(event.data) },
+                  onError: function (error) { Errors.postMessage(error.data) }
+              },
+          });
+      }
+
+      function hideAnnotations() {
+          document.body.style.height = '1000%';
+          document.body.style.width = '1000%';
+          document.body.style.transform = 'scale(0.1)';
+          document.body.style.transformOrigin = 'left top';
+          document.documentElement.style.height = '1000%';
+          document.documentElement.style.width = '1000%';
+          document.documentElement.style.transform = 'scale(0.1)';
+          document.documentElement.style.transformOrigin = 'left top';
+      }
+
+      function sendPlayerStateChange(playerState) {
+          clearTimeout(timerId);
+          StateChange.postMessage(playerState);
+          if (playerState == 1) {
+              startSendCurrentTimeInterval();
+              sendVideoData(player);
+          }
+      }
+
+      function sendVideoData(player) {
+          var videoData = {
+              'duration': player.getDuration(),
+              'videoUrl': player.getVideoUrl(),
+              'availableQualityLevels': player.getAvailableQualityLevels(),
+              'videoEmbedCode': player.getVideoEmbedCode(),
+          };
+          VideoData.postMessage(JSON.stringify(videoData));
+      }
+
+      function startSendCurrentTimeInterval() {
+          timerId = setInterval(function () {
+              CurrentTime.postMessage(player.getCurrentTime());
+              LoadedFraction.postMessage(player.getVideoLoadedFraction());
+          }, 100);
+      }
+
+      function play() {
+          player.playVideo();
+          return '';
+      }
+
+      function pause() {
+          player.pauseVideo();
+          return '';
+      }
+
+      function loadById(id, startAt) {
+          player.loadVideoById(id, startAt);
+          return '';
+      }
+
+      function cueById(id, startAt) {
+          player.cueVideoById(id, startAt);
+          return '';
+      }
+
+      function mute() {
+          player.mute();
+          return '';
+      }
+
+      function unMute() {
+          player.unMute();
+          return '';
+      }
+
+      function setVolume(volume) {
+          player.setVolume(volume);
+          return '';
+      }
+
+      function seekTo(position, seekAhead) {
+          player.seekTo(position, seekAhead);
+          return '';
+      }
+
+      function setSize(width, height) {
+          player.setSize(width, height);
+          return '';
+      }
+
+      function setPlaybackRate(rate) {
+          player.setPlaybackRate(rate);
+          return '';
+      }
+    </script>
+  </body>
+</html>
+    ''';
+  }
+  
+  String get iosYoutubeIframePlayer {
+    return 
+    '''
+<!DOCTYPE html>
+<html>
+  <head>
+    <style>
+      html, body {
+        height: 100%;
+        width: 100%;
+        margin: 0;
+        padding: 0;
+        background-color: #000000;
+        overflow: hidden;
+        position: fixed;
+      }
+    </style>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'>
+  </head>
+
+  <body>
+    <!-- 1. The <iframe> (and video player) will replace this <div> tag. -->
+    <div id="player"></div>
+
+    <script>
+      // 2. This code loads the IFrame Player API code asynchronously.
+      var tag = document.createElement('script');
+
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    </script>
+
+    <script type="text/javascript">
+      var player;
+      var timerId;
+      function onYouTubeIframeAPIReady() {
+          player = new YT.Player('player', {
+              height: '100%',
+              width: '100%',
+              host: 'https://www.youtube.com',
+              playerVars: {
+                  'controls': 0,
+                  'autoplay': 0,
+                  'playsinline': 1,
+                  'enablejsapi': 1,
+                  'fs': 0,
+                  'rel': 0,
+                  'showinfo': 0,
+                  'iv_load_policy': 3,
+                  'modestbranding': 1,
+                  'cc_load_policy': 0,
+              },
+              events: {
+                  onReady: function (event) { Ready.postMessage("Ready") },
+                  onStateChange: function (event) { sendPlayerStateChange(event.data) },
+                  onPlaybackQualityChange: function (event) { PlaybackQualityChange.postMessage(event.data) },
+                  onPlaybackRateChange: function (event) { PlaybackRateChange.postMessage(event.data) },
+                  onError: function (error) { Errors.postMessage(error.data) }
+              },
+          });
+      }
+
+      function sendPlayerStateChange(playerState) {
+          clearTimeout(timerId);
+          StateChange.postMessage(playerState);
+          if (playerState == 1) {
+              startSendCurrentTimeInterval();
+              sendVideoData(player);
+          }
+      }
+
+      function sendVideoData(player) {
+          var videoData = {
+              'duration': player.getDuration(),
+              'videoUrl': player.getVideoUrl(),
+              'availableQualityLevels': player.getAvailableQualityLevels(),
+              'videoEmbedCode': player.getVideoEmbedCode(),
+          };
+          VideoData.postMessage(JSON.stringify(videoData));
+      }
+
+      function startSendCurrentTimeInterval() {
+          timerId = setInterval(function () {
+              CurrentTime.postMessage(player.getCurrentTime());
+              LoadedFraction.postMessage(player.getVideoLoadedFraction());
+          }, 100);
+      }
+
+      function play() {
+          player.playVideo();
+          return '';
+      }
+
+      function pause() {
+          player.pauseVideo();
+          return '';
+      }
+
+      function loadById(id, startAt) {
+          player.loadVideoById(id, startAt);
+          return '';
+      }
+
+      function cueById(id, startAt) {
+          player.cueVideoById(id, startAt);
+          return '';
+      }
+
+      function mute() {
+          player.mute();
+          return '';
+      }
+
+      function unMute() {
+          player.unMute();
+          return '';
+      }
+
+      function setVolume(volume) {
+          player.setVolume(volume);
+          return '';
+      }
+
+      function seekTo(position, seekAhead) {
+          player.seekTo(position, seekAhead);
+          return '';
+      }
+
+      function setSize(width, height) {
+          player.setSize(width, height);
+          return '';
+      }
+
+      function setPlaybackRate(rate) {
+          player.setPlaybackRate(rate);
+          return '';
+      }
+    </script>
+  </body>
+</html>
+    ''';
   }
 }
